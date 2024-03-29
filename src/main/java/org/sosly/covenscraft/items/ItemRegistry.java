3914ba6cb9ceceb132ce2c437fc50f5775dfc963
Kevin Kragenbrink <kevin@writh.net>
2024-03-28 21:15:44 -0700

    feat(items): implement witch armor by @laniakea
    This is just the bare model, geometry, and textures.  There are a
    few clipping issues, and none of the language files, recipes, or
    even the inventory models are ready. But hey, it's in game!
    
    Related to #13.

---
+++ src/main/java/org/sosly/covenscraft/items/ItemRegistry.java
@@ -0,0 +1,34 @@
+package org.sosly.covenscraft.items;
+
+import com.mna.api.items.MACreativeTabs;
+import com.mna.items.armor.MAArmorMaterial;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.sosly.covenscraft.CovensCraft;
+import org.sosly.covenscraft.items.armor.CovenArmorItem;
+
+@Mod.EventBusSubscriber(modid= CovensCraft.MODID, bus= Mod.EventBusSubscriber.Bus.MOD)
+public class ItemRegistry {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CovensCraft.MODID);
+    public static final RegistryObject<Item> COVEN_ARMOR_CHEST = ITEMS.register("coven_armor_chest", () -> new CovenArmorItem(
+            MAArmorMaterial.INFUSED_SILK, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
+    public static final RegistryObject<Item> COVEN_ARMOR_LEGGINGS = ITEMS.register("coven_armor_leggings", () -> new CovenArmorItem(
+            MAArmorMaterial.INFUSED_SILK, ArmorItem.Type.LEGGINGS, new Item.Properties()));
+    public static final RegistryObject<Item> COVEN_ARMOR_HEAD = ITEMS.register("coven_armor_helmet", () -> new CovenArmorItem(
+            MAArmorMaterial.INFUSED_SILK, ArmorItem.Type.HELMET, new Item.Properties()));
+    public static final RegistryObject<Item> COVEN_ARMOR_BOOTS = ITEMS.register("coven_armor_boots", () -> new CovenArmorItem(
+            MAArmorMaterial.INFUSED_SILK, ArmorItem.Type.BOOTS, new Item.Properties()));
+
+    @SubscribeEvent
+    public static void FillCreativeTabs(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTab() == MACreativeTabs.GENERAL) {
+            ITEMS.getEntries().stream().map(RegistryObject::get).forEach(event::accept);
+        }
+    }
+}
