7816b31bbadcdce13dfebb5c78af67faa733d9a5
Kevin Kragenbrink <kevin@writh.net>
2024-04-03 10:40:59 -0700

    feat(config): add a new config value to block sympathy in boss arenas
    If the configuration value is set to true, targets in boss arenas will be fully protected from sympathetic magic.  This setting defaults to true.
    
    Resolves #26.

--- src/main/java/org/sosly/witchcraft/rituals/effects/SympathyRitual.java
+++ src/main/java/org/sosly/witchcraft/rituals/effects/SympathyRitual.java
@@ -3,14 +3,20 @@ package org.sosly.witchcraft.rituals.effects;
 import com.mna.api.capabilities.IPlayerProgression;
 import com.mna.api.rituals.IRitualContext;
 import com.mna.api.rituals.RitualEffect;
+import com.mna.api.spells.ComponentApplicationResult;
 import com.mna.api.spells.ICanContainSpell;
+import com.mna.api.spells.base.IModifiedSpellPart;
 import com.mna.api.spells.base.ISpellDefinition;
+import com.mna.api.spells.parts.Shape;
+import com.mna.api.spells.parts.SpellEffect;
 import com.mna.api.spells.targeting.SpellContext;
 import com.mna.api.spells.targeting.SpellSource;
 import com.mna.api.spells.targeting.SpellTarget;
+import com.mna.api.tools.MATags;
 import com.mna.capabilities.playerdata.progression.PlayerProgressionProvider;
 import com.mna.items.filters.SpellItemFilter;
 import com.mna.spells.SpellsInit;
+import com.mna.tools.StructureUtils;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
@@ -19,9 +25,11 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.sosly.witchcraft.Config;
 import org.sosly.witchcraft.Witchcraft;
 import org.sosly.witchcraft.factions.FactionRegistry;
 import org.sosly.witchcraft.utils.SympathyHelper;
@@ -71,6 +79,7 @@ public class SympathyRitual extends RitualEffect {
         ItemStack spellItem = getSpellItem(ctx);
         ItemStack boundPoppet = getBoundPoppet(ctx);
         if (spellItem.isEmpty() || boundPoppet.isEmpty()) {
+            player.sendSystemMessage(Component.translatable("rituals.error.no_target"));
             return false;
         }
 
@@ -81,23 +90,31 @@ public class SympathyRitual extends RitualEffect {
 
         Entity target = SympathyHelper.getBoundEntity(boundPoppet, level);
         if (target == null) {
+            player.sendSystemMessage(Component.translatable("rituals.error.no_target"));
+            return false;
+        }
+
+        if (Config.bossesBlockSympathy && isInBossArena(level, target)) {
+            player.sendSystemMessage(Component.translatable("rituals.sympathy.target_protected"));
             return false;
         }
 
         ICanContainSpell spellItemCapability = (ICanContainSpell) spellItem.getItem();
         ISpellDefinition spell = spellItemCapability.getSpell(spellItem, player);
-        if (spell.getShape().equals(SpellsInit.SELF)) {
+        if (spell.getShape() != null && isSelfSpell(spell.getShape().getPart())) {
+            player.sendSystemMessage(Component.translatable("rituals.sympathy.no_self_spells"));
             return false;
         }
 
+        IModifiedSpellPart<Shape> spellShape = spell.getShape();
         SpellSource spellSource = new SpellSource(player, InteractionHand.MAIN_HAND);
         SpellTarget spellTarget = new SpellTarget(target);
         SpellContext spellContext = new SpellContext(level, spell);
 
-        spell.iterateComponents(c ->  {
-            LOGGER.info("{} cast {} on {}.", player.getName().getString(), c.getPart().getRegistryName(),
+        spell.iterateComponents(component ->  {
+            LOGGER.info("{} cast {} on {}.", player.getName().getString(), component.getPart().getRegistryName(),
                     target.getName().getString());
-            c.getPart().ApplyEffect(spellSource, spellTarget, c, spellContext);
+            affectTarget(level, spellShape, spellSource, spellTarget, component, spellContext);
         });
 
         return true;
@@ -121,5 +138,18 @@ public class SympathyRitual extends RitualEffect {
                 .filter(filter::IsValidItem)
                 .findFirst()
                 .orElse(ItemStack.EMPTY);
+    }
+
+    private boolean isInBossArena(ServerLevel level, Entity target) {
+        return StructureUtils.isPointInAnyStructure(level, target.blockPosition(), MATags.Structures.BOSS_ARENAS);
+    }
+
+    private boolean isSelfSpell(Shape part) {
+        return part.equals(SpellsInit.SELF) || part.equals(SpellsInit.BOUND_AXE) || part.equals(SpellsInit.BOUND_SWORD)
+                || part.equals(SpellsInit.BOUND_BOW) || part.equals(SpellsInit.BOUND_SHIELD);
+    }
+
+    private ComponentApplicationResult affectTarget(Level level, IModifiedSpellPart<Shape> shape, SpellSource source, SpellTarget target, IModifiedSpellPart<SpellEffect> effect, SpellContext ctx) {
+        return effect.getPart().ApplyEffect(source, target, effect, ctx);
     }
 }
